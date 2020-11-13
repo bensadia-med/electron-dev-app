398099278f290a377a2cea6438327def7de645bd
BENSAADIA MOHAMMED <mohammedbensadia195@gmail.com>
2020-11-13 14:52:56 +0100

    BrowserWindow - Window State

--- main.js
+++ main.js
@@ -1,53 +1,35 @@
 // Modules
 const {app, BrowserWindow} = require('electron')
+const windowStateKeeper = require('electron-window-state')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow, secWindow
+let mainWindow
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow () {
 
-  mainWindow = new BrowserWindow({
-    width: 1000, height: 800,
-    minWidth: 300, minHeight: 150,
-    webPreferences: { nodeIntegration: true }
+  // Window state manager
+  let winState = windowStateKeeper({
+    defaultWidth: 1000, defaultHeight: 800
   })
 
-  secWindow = new BrowserWindow({
-    width: 700, height: 800,
+  mainWindow = new BrowserWindow({
+    width: winState.width, height: winState.height,
+    x: winState.x, y: winState.y,
     webPreferences: { nodeIntegration: true }
   })
 
   // Load index.html into the new BrowserWindow
   mainWindow.loadFile('index.html')
-  secWindow.loadFile('index.html')
 
   // Open DevTools - Remove for PRODUCTION!
   // mainWindow.webContents.openDevTools();
 
-  secWindow.on('closed', () => {
-    mainWindow.maximize()
-  })
-
-  // mainWindow.on('focus', () => {
-  //   console.log('Main win focused')
-  // })
-  // secWindow.on('focus', () => {
-  //   console.log('Second win focused')
-  // })
-  //
-  // app.on('browser-window-focus', () => {
-  //   console.log('App focused')
-  // })
-  //
-  // console.log(BrowserWindow.getAllWindows())
+  winState.manage(mainWindow)
 
   // Listen for window being closed
   mainWindow.on('closed',  () => {
-    mainWindow = null
-  })
-  secWindow.on('closed',  () => {
     mainWindow = null
   })
 }
