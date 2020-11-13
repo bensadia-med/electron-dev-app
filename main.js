6b738609981745bfa1c76d106315650c986ab310
BENSAADIA MOHAMMED <mohammedbensadia195@gmail.com>
2020-11-13 21:46:34 +0100

    Features & Techniques - Network Detection

--- main.js
+++ main.js
@@ -1,50 +1,28 @@
 // Modules
 const {app, BrowserWindow} = require('electron')
-const fs = require('fs')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
-
-app.disableHardwareAcceleration()
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow () {
 
   mainWindow = new BrowserWindow({
     width: 1000, height: 800,
-    show: false,
-    webPreferences: {
-      nodeIntegration: true,
-      offscreen: true
-    }
+    webPreferences: { nodeIntegration: true }
   })
 
   // Load index.html into the new BrowserWindow
-  mainWindow.loadURL('https://electronjs.org')
-
-  let i = 1
-  mainWindow.webContents.on('paint', (e, dirty, image) => {
-
-    let screenshot = image.toPNG()
-    fs.writeFile( app.getPath('desktop') + `/screenshot_${i}.png`, screenshot, console.log )
-    i++
-  })
-
-  mainWindow.webContents.on('did-finish-load', e => {
-    console.log( mainWindow.getTitle() )
-
-    mainWindow.close()
-    mainWindow = null
-  })
+  mainWindow.loadFile('index.html')
 
   // Open DevTools - Remove for PRODUCTION!
-  // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 
   // Listen for window being closed
-  // mainWindow.on('closed',  () => {
-  //   mainWindow = null
-  // })
+  mainWindow.on('closed',  () => {
+    mainWindow = null
+  })
 }
 
 // Electron `app` is ready
