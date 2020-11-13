0ff24dbb228c0b607695366b851b16b7e0730a76
BENSAADIA MOHAMMED <mohammedbensadia195@gmail.com>
2020-11-13 14:52:01 +0100

    BrowserWindow - Properties & Methodes

--- main.js
+++ main.js
@@ -3,26 +3,51 @@ const {app, BrowserWindow} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow, secWindow
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow () {
 
   mainWindow = new BrowserWindow({
     width: 1000, height: 800,
-    frame: false,
-    titleBarStyle: 'hidden',
+    minWidth: 300, minHeight: 150,
+    webPreferences: { nodeIntegration: true }
+  })
+
+  secWindow = new BrowserWindow({
+    width: 700, height: 800,
     webPreferences: { nodeIntegration: true }
   })
 
   // Load index.html into the new BrowserWindow
   mainWindow.loadFile('index.html')
+  secWindow.loadFile('index.html')
 
   // Open DevTools - Remove for PRODUCTION!
   // mainWindow.webContents.openDevTools();
 
+  secWindow.on('closed', () => {
+    mainWindow.maximize()
+  })
+
+  // mainWindow.on('focus', () => {
+  //   console.log('Main win focused')
+  // })
+  // secWindow.on('focus', () => {
+  //   console.log('Second win focused')
+  // })
+  //
+  // app.on('browser-window-focus', () => {
+  //   console.log('App focused')
+  // })
+  //
+  // console.log(BrowserWindow.getAllWindows())
+
   // Listen for window being closed
   mainWindow.on('closed',  () => {
+    mainWindow = null
+  })
+  secWindow.on('closed',  () => {
     mainWindow = null
   })
 }
